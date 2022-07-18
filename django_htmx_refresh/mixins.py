dafd47f0b12d7688133251a8be9c16a42a16e629
JohnGWebDev <john1galiszewski@gmail.com>
2022-07-18 18:25:22 -0400

    update mixins

--- django_htmx_refresh/mixins.py
+++ django_htmx_refresh/mixins.py
@@ -1,9 +1,9 @@
 from django.urls import resolve
 from django.views.generic.base import TemplateResponseMixin
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import ContextMixin
 
 
-class HtmxResponseMixin(LoginRequiredMixin, TemplateResponseMixin):
+class HtmxResponseMixin(TemplateResponseMixin, ContextMixin):
     """Extend Django's default TemplateResponseMixin to handle HTMX requests."""
 
     def get_template_names(self):
@@ -11,6 +11,12 @@ class HtmxResponseMixin(LoginRequiredMixin, TemplateResponseMixin):
         if there is an HTMX instance attached to it
         to return the appropriate template."""
         app_name = resolve(self.request.path).app_name
-        if self.request.htmx:
-            return [f"{app_name}/partials/{self.template_name}"]
         return [f"{app_name}/{self.template_name}"]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.htmx:
+            context["base_template"] = "partial.html"
+        else:
+            context["base_template"] = "base.html"
+        return context
